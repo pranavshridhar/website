5f37c8e538646ec0ee93a3c16261c79f5257bd21
Pranav Shridhar <pranavshridhar0@gmail.com>
2025-03-03 17:57:09 -0600

    Under construction notice

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ function App() {
 
 
   return (
-    <h1>Pranav here!!!</h1>
-  )
+    <>
+      <h1>Pranav here!!!</h1>
+      <h1>This page is still under construction, but check back later.</h1>
+    </>
+  );
 }
 
 export default App
