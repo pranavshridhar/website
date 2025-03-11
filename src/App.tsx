7a7cb3e40f1457146f986fe740cf41cd6e6c7779
Pranav Shridhar <pranavshridhar0@gmail.com>
2025-03-11 18:38:15 -0500

    Added photo to public at least

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <>
     <div className="text-center">
-      <h1 className="">Pranav here!!!</h1>
-      <p className="font-mono">This page is still under construction, but check back later.</p>
+      <h1 className="text-blue-600">Pranav here!!!</h1>
+      <p className="text-red-600">This page is still under construction, but check back later.</p>
     </div>
      
     </>
